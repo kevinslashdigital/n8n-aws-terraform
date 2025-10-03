8b0540e1bbef4eaa0c2ddf62756d940ac2e70821
yinseng <kevin@slash.co>
2025-10-03 14:07:59 +0700

    feat: add ALB DNS name and n8n URL outputs to production environment

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -17,3 +17,13 @@ output "private_subnet_ids" {
   description = "IDs of the private subnets"
   value       = module.networking.private_subnet_ids
 }
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.alb.alb_dns_name
+}
+
+output "n8n_url" {
+  description = "URL to access n8n"
+  value       = var.certificate_arn != "" ? "https://${var.domain_name != "" ? var.domain_name : module.alb.alb_dns_name}" : "http://${module.alb.alb_dns_name}"
+}
